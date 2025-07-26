dcb13cbcbb6175b25085fb9138f4f4463675883e
Luis Ramirez <luis.ramirez@teradata.com>
2025-07-25 21:08:52 -0500

    Update

--- netlify/functions/app.py
+++ netlify/functions/app.py
@@ -35,8 +35,12 @@ def index():
 
     return render_template("index.html", resultado=resultado, fig=fig_json)
 
-if __name__ == "__main__":
-    #app.run(debug=True)
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Render asigna un puerto autom·ticamente
-    app.run(debug=False, host="0.0.0.0", port=port)
+# Esta es la función que Netlify ejecutará en cada petición
+def handler(event, context):
+    return handle(app, event, context)
+
+#if __name__ == "__main__":
+#    #app.run(debug=True)
+#    import os
+#    port = int(os.environ.get("PORT", 5000))  # Render asigna un puerto autom·ticamente
+#    app.run(debug=False, host="0.0.0.0", port=port)
